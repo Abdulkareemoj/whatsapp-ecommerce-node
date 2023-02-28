0c6f2b8379386fd38c48186a2a8113ebbbec2e8e
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-02-28 12:22:20 +0000

    chore: :tada:
    initial commit

--- .env.js
+++ .env.js
@@ -1,22 +1,3 @@
-const production = {
-    ...process.env,
-    NODE_ENV: process.env.NODE_ENV || 'production',
-};
-
-const development = {
-    ...process.env,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: '9000',
-    Meta_WA_accessToken:'',
-    Meta_WA_SenderPhoneNumberId: '',
-    Meta_WA_wabaId: '',
-    Meta_WA_VerifyToken: '',
-};
-
-const fallback = {
-    ...process.env,
-    NODE_ENV: undefined,
-};
 
 module.exports = (environment) => {
     console.log(`Execution environment selected is: "${environment}"`);
